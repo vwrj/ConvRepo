36a17c76087e88c8d1bfa4bcb756b2f8d1c425c2
Vish Rajiv <vishwaesh.rajiv@gmail.com>
2019-10-07 20:40:09 -0400

    can now run pre-trained RotNet + classifier, testing transfer learning of each convolutional block from Rotnet on CIFAR-10

--- rotation_classifier_results.py
+++ rotation_classifier_results.py
@@ -22,6 +22,7 @@ batch_size = 256 # input batch size for training
 epochs = 100       # number of epochs to train
 lr = 0.1      # learning rate
 momentum = 0.9
+nesterov = True
 weight_decay = 5e-4
 
 data_transform = transforms.Compose([
@@ -146,16 +147,24 @@ class NetworkInNetwork(pl.LightningModule):
     
     
 
-
 class PretrainedRotNetClassifier(pl.LightningModule):
 
-    def __init__(self, pretrained_model):
+    def __init__(self, args, pretrained_model):
         super(PretrainedRotNetClassifier, self).__init__()
+
+        # RotNet_B3
+        self.pretrained_model = pretrained_model
+        self.blocks = args.blocks
+
+        if args.blocks==1:
+            num_flatten_feats = 96 * 16 * 16
+        else:
+            num_flatten_feats = 192 * 8 * 8
         
         self.layers = nn.Sequential(OrderedDict([
-            ('RotNet_B3', pretrained_model),
+           # ('RotNet_B3', pretrained_model),
             ('flatten', nn.Flatten()),
-            ('linear_1', nn.Linear(192 * 8 * 8, 200)),
+            ('linear_1', nn.Linear(num_flatten_feats, 200)),
             ('batchnorm', nn.BatchNorm1d(200)),
             ('relu', nn.ReLU(inplace=True)),
             ('linear_2', nn.Linear(200, 200)),
@@ -166,6 +175,8 @@ class PretrainedRotNetClassifier(pl.LightningModule):
         
         
     def forward(self, x):
+        for block in self.pretrained_model:
+            x = block(x)
         return self.layers(x)
 
     def training_step(self, batch, batch_nb):
@@ -180,7 +191,7 @@ class PretrainedRotNetClassifier(pl.LightningModule):
         pred = y_hat.data.max(1, keepdim=True)[1] # get the index of the max log-probability
         correct = pred.eq(y.data.view_as(pred)).sum()
 
-        pdb.set_trace()
+        # pdb.set_trace()
         
         return {'val_loss': F.cross_entropy(y_hat, y), 'correct': correct, 'bs': y.shape[0]}
 
@@ -193,7 +204,19 @@ class PretrainedRotNetClassifier(pl.LightningModule):
     def configure_optimizers(self):
         # REQUIRED
         # can return multiple optimizers and learning_rate schedulers
-        return torch.optim.SGD(filter(lambda p: p.requires_grad, self.parameters()), lr=lr, momentum=momentum, weight_decay=weight_decay)
+        opt = torch.optim.SGD(filter(lambda p: p.requires_grad, self.parameters()), lr=lr, momentum=momentum, weight_decay=weight_decay, nesterov=nesterov)
+        def adjust_lr(epoch):
+            factor = 0.2
+            if epoch == 40:
+                return factor 
+            if epoch == 45:
+                return factor ** 2
+            if epoch == 50:
+                return factor ** 3
+            return 1
+
+        scheduler = torch.optim.lr_scheduler.LambdaLR(opt, lr_lambda = adjust_lr)
+        return [opt], [scheduler]
 
     @pl.data_loader
     def tng_dataloader(self):
@@ -206,21 +229,52 @@ class PretrainedRotNetClassifier(pl.LightningModule):
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser(description='Test transfer learning performance of (each convolutional block in) Rotation Net + classifier on CIFAR-10')
+    parser.add_argument('--blocks', type=int, action='store', help='the desired convolutional block to extract', default=3, choices = range(1, 4))
+
+    args = parser.parse_args()
+
+    print("Using convolutional block {} from pre-trained RotNet model".format(args.blocks))
+
     model = NetworkInNetwork()
     model.load_state_dict(torch.load("best_rot_model_100/_ckpt_epoch_61.ckpt")['state_dict'])
 
-    # TODO TODO TODO: Find out how to remove last Global Avg Pool and Classifier layers from 3rd Convolutional Block in Network-in-Network
-    new_model = nn.Sequential(*list(model.children())[:-1])
-    
-    for c in model.children():
-        pdb.set_trace()
-        print(5)
+    new_blocks = []
+    module_list = list(list(model.children())[0].children())
 
-    '''
-    for param in model.parameters():
+    for i in range(args.blocks):
+        if i == 2:
+            # Remove last two layers (Global Avg Pool and Classifier) from third Convolutional Block in Network-in-Network
+            new_blocks.append(nn.Sequential(*list(module_list[i].children())[:-2]))
+        else:
+            new_blocks.append(module_list[i])
+
+    new_model = nn.ModuleList(new_blocks)
+    print("Pre-trained RotNet model now has {} blocks".format(len(new_model)))
+    for param in new_model.parameters():
         param.requires_grad = False
 
-    classifier = PretrainedRotNetClassifier(pretrained_model=model)
+
+
+    # module_list = list(list(model.children())[0].children())
+    # new_blocks.append(module_list[0])
+    # new_blocks.append(module_list[1])
+    # new_blocks.append(nn.Sequential(*list(module_list[2].children())[:-2]))
+
+    # new_model = nn.ModuleList(new_blocks)
+    # for param in new_model.parameters():
+    #     param.requires_grad = False
+
+    classifier = PretrainedRotNetClassifier(args, pretrained_model = new_model)
+    exp = Experiment(save_dir='./rot_classifier_logs')
+    trainer = Trainer(experiment=exp, gpus=[0], max_nb_epochs=epochs) 
+    trainer.fit(classifier)
+
+    # pdb.set_trace()
+    
+    # print(5)
+
+    '''
     exp = Experiment(save_dir='./rot_classifier_logs')
     # trainer = Trainer(experiment=exp, gpus=[0], max_nb_epochs=epochs) 
     trainer = Trainer(experiment=exp, max_nb_epochs=epochs) 
